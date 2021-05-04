5edff5bcd639dbec20d91ccf6b456c2296332222
Ömer Faruk Genç <omergnc06@gmail.com>
2021-05-05 02:32:39 +0300

    Proje dosyası ekle.

---
+++ WebApplication1/Controllers/CariPanelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.Siniflar;
+
+namespace WebApplication1.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        Context c = new Context();
+        [Authorize]     
+        public ActionResult Index()
+        {
+            var mail = (string)Session["CariMail"];
+            var pnl = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+            ViewBag.neden = mail;
+            return View(pnl);
+        }
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id = c.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.Cariid).FirstOrDefault();
+            var dgr = c.SatisHarekets.Where(x => x.Cariid == id).ToList();
+            return View(dgr);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/DepartmanController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models.Siniflar;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departmen.Where(x => x.Durum == true).ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult DepartmanEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DepartmanEkle(Departman d)
+        {
+            c.Departmen.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanSil(int id)
+        {
+            var dep = c.Departmen.Find(id);
+            dep.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanGetir(int id)
+        {
+            var dpt = c.Departmen.Find(id);
+            return View("DepartmanGetir" , dpt);
+
+        }
+        public ActionResult DepartmanGuncelle(Departman k)
+        {
+            var ktgr = c.Departmen.Find(k.Departmanid);
+            ktgr.DepartmanAd = k.DepartmanAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DepartmanDetay(int id)
+        {
+            var degerler = c.Personels.Where(x => x.Departmanid == id).ToList();
+            var dpt = c.Departmen.Where(x => x.Departmanid == id).Select(y => y.DepartmanAd).FirstOrDefault();
+            ViewBag.d = dpt;
+            return View(degerler);
+        }
+        public ActionResult DepartmanPersonelSatis(int id)
+        {
+            var degerler = c.SatisHarekets.Where(x => x.Personelid == id).ToList();
+            var per = c.Personels.Where(x => x.Personelid == id).Select(y => y.PersonelAd + " "  + y.PersonelSoyad).FirstOrDefault();
+            ViewBag.dpers = per;
+            return View(degerler);
+        }
+    }
+   
+}
